83df087cce5da1db95b6a8a5a33e4fe8135f2453
Stanislav Zdych <zdych.stanislav@gmail.com>
2021-10-12 23:19:35 +0200

    Main Menu created

---
+++ script.js
@@ -0,0 +1,41 @@
+let clickElement = $("#clickElement");
+let hoverElement = $("#hoverElement");
+let musicElement = $("#musicElement");
+
+let musicControl = $("musicControl");
+let audioControl = $("#audioControl");
+
+
+let buttons = document.querySelectorAll(".button");
+buttons.forEach(button => {
+    console.log(button);
+    button.addEventListener("mouseover", PlayHover);
+    button.addEventListener("click", PlayClick);
+})
+
+function PlayHover(){
+    hoverElement.currentTime = 0;
+    hoverElement.play();
+}
+
+function PlayClick(){
+    clickElement .currentTime = 0;
+    clickElement .play();
+}
+
+function ChangeMusicVolume(event){
+    musicElement.volume = event.target.value;
+}
+
+function ChangeAudioVolume(event){
+    hoverElement.volume = event.target.value;
+    clickElement.volume = event.target.value;
+}
+
+function ClosePopUp(a){
+    a.closest(".popup").style.display = "none";
+}
+
+function OpenPopUp(id){
+    $(id).style.display = "grid";
+}
