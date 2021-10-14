2fccc6fc199204e727cee732f5c44e5b507ad6e2
Stanislav Zdych <zdych.stanislav@gmail.com>
2021-10-14 17:46:20 +0200

    Resource converted to class

---
+++ classes/resource.js
@@ -0,0 +1,49 @@
+class Resource{
+    constructor(name, startValue, viewContainer){
+        let desc = $N("p");
+        let val = $N("p");
+
+        this.name = name;
+        this.viewElement = val;
+        this.value = startValue;      
+
+        desc.innerHTML = name;
+        this.GenerateHTMLView(desc,val,viewContainer);
+    }
+
+    Add(value){
+        this.value += value;
+        this.RefreshView();
+    }
+
+    Substract(value) {
+        this.value -= value;
+        this.RefreshView();
+    }
+
+    Modify(value) {
+        this.value = value;
+        this.RefreshView();
+    }
+
+    RefreshView(){
+        if(this.value > 999999){
+            this.viewElement.innerHTML = (this.value/1000000).toFixed(1) + "M";
+        }else if(this.value > 99999){
+            this.viewElement.innerHTML = (this.value/1000).toFixed(1)  + "k";
+        } else {
+            this.viewElement.innerHTML = this.value;
+        }       
+        console.log("resource refresh");
+    }
+
+    GenerateHTMLView(desc, val, container){
+        let span = $N("span");
+        
+        container.append(span);
+        span.append(desc);
+        span.append(val);
+        span.classList.add("resource");
+        this.RefreshView();
+    }
+}
--- economy.js
+++ economy.js
@@ -1,14 +1,14 @@
-let influence = 100;
+/*let influence = 100;
 let population = 13410750;
 let troops = 0;
 let materials = 0;
 let oil = 0;
 let mines = 0;
-let refineries = 0;
+let refineries = 0;*/
 
 let resourceDiv = $("#resources");
 
-let influenceControl = UIResource("Political Influence", influence);
+/*let influenceControl = UIResource("Political Influence", influence);
 let populationControl = UIResource("Population", population);
 let troopsControl = UIResource("Mobilized Troops", troops);
 let materialsControl = UIResource("Materials", materials);
@@ -20,19 +20,24 @@ let refineriesControl = UIResource("Refineries", refineries);
 console.log(influenceControl);
 
 function UIResource(name, value){
-    let span = $N("span");
-    let pDesc = $N("p");
-    let pVal = $N("p");
-
-    pDesc.innerHTML = name;
-    pVal.innerHTML = value;
     
-    resourceDiv.append(span);
-    span.append(pDesc);
-    span.append(pVal);
-    span.classList.add("resource");
 
     return pVal;
-}
+}*/
 //TODO FIX THIS
 //AddResource()
+
+let influence = new Resource("Political Influence", 0, resourceDiv);
+let population = new Resource("Population", 13410750, resourceDiv);
+let troops = new Resource("Mobilized Troops", 0, resourceDiv);
+let materials = new Resource("Materials", 0, resourceDiv);
+let oil = new Resource("Oil", 0, resourceDiv);
+let mines = new Resource("Mines", 0, resourceDiv);
+let refineries = new Resource("Refineries", 0, resourceDiv);
+let barracks = new Resource("Barracks", 0, resourceDiv);
+let factories = new Resource("Factories", 0, resourceDiv);
+
+setInterval(function(){
+    //console.log("resources added");
+    influence.Add(1);
+}, 1000)
