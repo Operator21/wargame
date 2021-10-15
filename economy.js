36cd354dbbf9a02f53b84ec9ec2edcf491168c45
Stanislav Zdych <zdych.stanislav@gmail.com>
2021-10-15 13:29:57 +0200

    Military strength added WIP

--- economy.js
+++ economy.js
@@ -2,7 +2,8 @@ let resourceDiv = $("#resources");
 
 let influence = new Resource("Political Influence", 0, resourceDiv);
 let population = new Resource("Population", 13410750, resourceDiv);
-let troops = new Resource("Mobilized Troops", 0, resourceDiv);
+let infantry = new Resource("Infantry Strength", 0, resourceDiv);
+let armor = new Resource("Armored Strength", 0, resourceDiv);
 let materials = new Resource("Materials", 1000, resourceDiv);
 let oil = new Resource("Oil", 0, resourceDiv);
 let mines = new Resource("Mines", 0, resourceDiv);
--- production.js
+++ production.js
@@ -7,6 +7,6 @@ let refineryView = new UnitView("refinery", "building", constructionView, [new R
 let barracksView = new UnitView("barracks", "building", constructionView, [new ResourceAmount(population, 40), new ResourceAmount(materials, 1000)], [new ResourceAmount(barracks, 1)]);
 let factoryView = new UnitView("factory", "building", constructionView, [new ResourceAmount(population, 500), new ResourceAmount(materials, 5000)], [new ResourceAmount(factories, 1)]);
 
-let infantryView = new UnitView("infantry", "unit", trainingView);
-let tankView = new UnitView("tank", "unit", trainingView);
+let infantryView = new UnitView("infantry", "unit", trainingView, [new ResourceAmount(population, 10000), new ResourceAmount(materials, 2500)], [new ResourceAmount(infantry, 10000)]);
+let tankView = new UnitView("tank", "unit", trainingView, [new ResourceAmount(population, 2000), new ResourceAmount(materials, 4000), new ResourceAmount(oil, 500)], [new ResourceAmount(armor, 500)]);
 
