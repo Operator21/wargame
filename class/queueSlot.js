793ef2103e4f2ae3b588379bd46af51e3262e1b8
Stanislav Zdych <zdych.stanislav@gmail.com>
2021-11-27 15:58:50 +0100

    videos added

--- class/queueSlot.js
+++ class/queueSlot.js
@@ -6,17 +6,33 @@ class QueueSlot {
         this.queue = queue;
 
         this.span = $N("span");
-        this.img = $N("img");
+        //this.img = $N("img");
         this.progressBar = $N("progress");
+        this.video = $N("video");
+        this.video.autoplay = true;
+        this.video.loop = true;
+
+        switch(unit.name){
+            case "infantry":
+                this.video.src = "video/train.webm";    
+                break;
+            case "tank":
+                this.video.src = "video/tank.webm";    
+                break;
+            default:
+                this.video.src = "video/crane.webm";               
+        }
+            
 
         this.span.classList.add("queueSlot");
-        this.img.src = "img/" + this.unit.type + "/" + this.unit.name + ".png";
+        //this.img.src = "img/" + this.unit.type + "/" + this.unit.name + ".png";
         
         console.log(this.progressBar);
         this.progressBar.value = 0;
         this.progressBar.max = this.max;
-        this.span.append(this.img);
+        //this.span.append(this.img);
         this.span.append(this.progressBar);
+        this.span.append(this.video);
         queueContainer.append(this.span);
     }
 
